1d0d6ec280dd6f164fd2cd24ab59abe12b1edf53
Oscar Prieto (PiRe) <opblanco@gmail.com>
2014-11-16 21:43:34 +0100

    p01 finished

--- p01.rkt
+++ p01.rkt
@@ -1,1 +1,24 @@
-; Find the last box of a list.
+;;;; Find the last box of a list.
+;;;; Example:
+;;;; * (my-last '(a b c d))
+;;;; (d)
+
+#lang racket
+
+(define (my-last lst)
+  (if (null? lst)
+      '()
+      (if (null? (cdr lst))
+          (car lst)
+          (my-last (cdr lst)))))
+
+; captain obvius :-)
+(define (my-last-2 lst)
+  (if (empty? lst)
+      '()
+      (last lst)))
+
+(my-last '(1 2))
+(my-last '())
+(my-last-2 '(1 2))
+(my-last-2 '())
